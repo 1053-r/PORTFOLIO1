e180d72bb3f3a78678076ae75225f8ec49e82af2
SOME ONE <Jassemghanmi123456789@gmail.com>
2022-10-23 22:07:23 +0100

    Services-section-is-added

---
+++ Javascript/navbarHamb.js
@@ -0,0 +1,5 @@
+const onClickFunc=()=>{
+    document.querySelector('.hamburger').classList.toggle('is-active');
+    document.querySelector('.hamb-links').classList.toggle('active-links');
+    document.querySelector('.hamb-bg').classList.toggle('active-bg');
+}
